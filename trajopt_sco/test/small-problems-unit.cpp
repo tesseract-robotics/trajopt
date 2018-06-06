803a578f2cbbaaffb66a1a68f7275d137c5692c5
Levi <levi.armstrong@gmail.com>
2018-06-06 13:25:25 -0500

    Fix trajopt_sco unit test

--- trajopt_sco/test/small-problems-unit.cpp
+++ trajopt_sco/test/small-problems-unit.cpp
@@ -45,12 +45,13 @@ TEST(SQP, QuadraticSeparable)
   setupProblem(prob, 3);
   prob->addCost(CostPtr(new CostFromFunc(ScalarOfVector::construct(&f_QuadraticSeparable), prob->getVars(), "f")));
   BasicTrustRegionSQP solver(prob);
-  solver.trust_box_size_ = 100;
-  vector<double> x = list_of(3)(4)(5);
+  BasicTrustRegionSQPParameters &params = solver.getParameters();
+  params.trust_box_size = 100;
+  vector<double> x = {3, 4, 5};
   solver.initialize(x);
   OptStatus status = solver.optimize();
   ASSERT_EQ(status, OPT_CONVERGED);
-  expectAllNear(solver.x(), list_of(0)(1)(2), 1e-3);
+  expectAllNear(solver.x(), {0, 1, 2}, 1e-3);
   // todo: checks on number of iterations and function evaluates
 }
 double f_QuadraticNonseparable(const VectorXd& x) { return sq(x(0) - x(1) + 3 * x(2)) + sq(x(0) - 1) + sq(x(2) - 2); }
@@ -61,14 +62,15 @@ TEST(SQP, QuadraticNonseparable)
   prob->addCost(
       CostPtr(new CostFromFunc(ScalarOfVector::construct(&f_QuadraticNonseparable), prob->getVars(), "f", true)));
   BasicTrustRegionSQP solver(prob);
-  solver.trust_box_size_ = 100;
-  solver.min_trust_box_size_ = 1e-5;
-  solver.min_approx_improve_ = 1e-6;
-  vector<double> x = list_of(3)(4)(5);
+  BasicTrustRegionSQPParameters &params = solver.getParameters();
+  params.trust_box_size = 100;
+  params.min_trust_box_size = 1e-5;
+  params.min_approx_improve = 1e-6;
+  vector<double> x = {3, 4, 5};
   solver.initialize(x);
   OptStatus status = solver.optimize();
   ASSERT_EQ(status, OPT_CONVERGED);
-  expectAllNear(solver.x(), list_of(1)(7)(2), .01);
+  expectAllNear(solver.x(), {1, 7, 2}, .01);
   // todo: checks on number of iterations and function evaluates
 }
 
@@ -85,10 +87,11 @@ void testProblem(ScalarOfVectorPtr f,
   prob->addCost(CostPtr(new CostFromFunc(f, prob->getVars(), "f", true)));
   prob->addConstraint(ConstraintPtr(new ConstraintFromFunc(g, prob->getVars(), VectorXd(), cnt_type, "g")));
   BasicTrustRegionSQP solver(prob);
-  solver.max_iter_ = 1000;
-  solver.min_trust_box_size_ = 1e-5;
-  solver.min_approx_improve_ = 1e-10;
-  solver.merit_error_coeff_ = 1;
+  BasicTrustRegionSQPParameters &params = solver.getParameters();
+  params.max_iter = 1000;
+  params.min_trust_box_size = 1e-5;
+  params.min_approx_improve = 1e-10;
+  params.merit_error_coeff = 1;
 
   solver.initialize(init);
   OptStatus status = solver.optimize();
@@ -136,19 +139,19 @@ VectorXd g_TP7(const VectorXd& x)
 TEST(SQP, TP1)
 {
   testProblem(
-      ScalarOfVector::construct(&f_TP1), VectorOfVector::construct(&g_TP1), INEQ, list_of(-2)(1), list_of(1)(1));
+      ScalarOfVector::construct(&f_TP1), VectorOfVector::construct(&g_TP1), INEQ, {-2, 1}, {1, 1});
 }
 TEST(SQP, TP3)
 {
   testProblem(
-      ScalarOfVector::construct(&f_TP3), VectorOfVector::construct(&g_TP3), INEQ, list_of(10)(1), list_of(0)(0));
+        ScalarOfVector::construct(&f_TP3), VectorOfVector::construct(&g_TP3), INEQ, {10, 1}, {0, 0});
 }
 TEST(SQP, TP6)
 {
-  testProblem(ScalarOfVector::construct(&f_TP6), VectorOfVector::construct(&g_TP6), EQ, list_of(10)(1), list_of(1)(1));
+  testProblem(ScalarOfVector::construct(&f_TP6), VectorOfVector::construct(&g_TP6), EQ, {10, 1}, {1, 1});
 }
 TEST(SQP, TP7)
 {
   testProblem(
-      ScalarOfVector::construct(&f_TP7), VectorOfVector::construct(&g_TP7), EQ, list_of(2)(2), list_of(0.)(sqrtf(3.)));
+      ScalarOfVector::construct(&f_TP7), VectorOfVector::construct(&g_TP7), EQ, {2, 2}, {0., sqrtf(3.)});
 }
