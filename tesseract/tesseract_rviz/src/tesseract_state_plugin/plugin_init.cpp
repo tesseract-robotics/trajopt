2274f5a15bb5b068bec5d16865d5f46aeabdd7ba
Levi <levi.armstrong@gmail.com>
2018-06-06 10:04:59 -0500

    Move tesseract into its own repository

--- tesseract/tesseract_rviz/src/tesseract_state_plugin/plugin_init.cpp
+++
@@ -1,4 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include <tesseract_rviz/tesseract_state_plugin/tesseract_state_display.h>
-
-PLUGINLIB_EXPORT_CLASS(tesseract_rviz::TesseractStateDisplay, rviz::Display)
--- tesseract/tesseract_rviz/src/test.cpp
+++
@@ -1,2 +0,0 @@
-#include "tesseract_rviz/tesseract_trajectory_plugin/tesseract_trajectory_display.h"
-int main(int /*argc*/, char** /*argv[]*/) { tesseract_rviz::TesseractTrajectoryDisplay test; }
