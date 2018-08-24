1fc21c3f1314fb914abf436b34084a56b3145b43
Reid Christopher <reid.christopher@yahoo.com>
2018-08-24 14:22:45 -0500

    Clang format file writing callback

--- trajopt/include/trajopt/file_write_callback.hpp
+++ trajopt/include/trajopt/file_write_callback.hpp
@@ -1,12 +1,12 @@
 #pragma once
 #include <trajopt/problem_description.hpp>
 
-
 namespace trajopt
 {
-
-  Optimizer::Callback WriteCallback(std::shared_ptr<std::ofstream> file, TrajOptProbPtr prob, const vector<string>& joint_names,
-                                    Eigen::Affine3d tcp = Eigen::Affine3d::Identity(),
-                                    bool angle_axis = false, bool degrees = false);
-
+Optimizer::Callback WriteCallback(std::shared_ptr<std::ofstream> file,
+                                  TrajOptProbPtr prob,
+                                  const vector<string>& joint_names,
+                                  Eigen::Affine3d tcp = Eigen::Affine3d::Identity(),
+                                  bool angle_axis = false,
+                                  bool degrees = false);
 }
