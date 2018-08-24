e9e0d21b1978358e50bd957d06c41539b0cefbdf
Reid Christopher <reid.christopher@yahoo.com>
2018-08-24 13:41:37 -0500

    Removed pose inverses/errors and changed file name arg to ofstream object in file writing utility

--- trajopt/include/trajopt/file_write_callback.hpp
+++ trajopt/include/trajopt/file_write_callback.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <trajopt/problem_description.hpp>
 
+
 namespace trajopt
 {
 
-  Optimizer::Callback WriteCallback(string file_name,TrajOptProbPtr prob, vector<string>& joint_names,
-                                    EigenSTL::vector_Affine3d pose_inverses, Eigen::Affine3d tcp = Eigen::Affine3d::Identity(),
-                                    bool angle_axis = true, bool degrees = true);
+  Optimizer::Callback WriteCallback(std::shared_ptr<std::ofstream> file, TrajOptProbPtr prob, const vector<string>& joint_names,
+                                    Eigen::Affine3d tcp = Eigen::Affine3d::Identity(),
+                                    bool angle_axis = false, bool degrees = false);
 
 }
