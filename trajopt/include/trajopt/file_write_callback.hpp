7b23087044bf55f8cf084537cf12ba654e76d2e2
Matthew Powelson <matthew.powelson@swri.org>
2018-10-19 07:56:51 -0500

    File write callback - Change affine3d to isometry3d

--- trajopt/include/trajopt/file_write_callback.hpp
+++ trajopt/include/trajopt/file_write_callback.hpp
@@ -6,7 +6,7 @@ namespace trajopt
 Optimizer::Callback WriteCallback(std::shared_ptr<std::ofstream> file,
                                   TrajOptProbPtr prob,
                                   const vector<string>& joint_names,
-                                  Eigen::Affine3d tcp = Eigen::Affine3d::Identity(),
+                                  Eigen::Isometry3d tcp = Eigen::Isometry3d::Identity(),
                                   bool angle_axis = false,
                                   bool degrees = false);
 }
