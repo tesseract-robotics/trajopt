7b67933fe8c4ab44e0643718621a53006ba286d7
Matthew Powelson <matthew.powelson@swri.org>
2018-10-29 12:41:05 -0500

    File Write Callback: Add License Info

--- trajopt/src/file_write_callback.cpp
+++ trajopt/src/file_write_callback.cpp
@@ -1,3 +1,29 @@
+/*
+Copyright 2018, Southwest Research Institute
+
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met :
+
+    1. Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation and / or
+        other materials provided with the distribution.
+
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO,
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.IN
+        NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+    OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+                             LOSS OF USE, DATA, OR PROFITS;
+                             OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+    WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    */
 #include <functional>
 #include <fstream>
 #include <trajopt/file_write_callback.hpp>
@@ -68,8 +94,7 @@ void WriteFile(shared_ptr<ofstream> file,
   *file << endl;
 }  // namespace trajopt
 
-Optimizer::Callback WriteCallback(shared_ptr<ofstream> file,
-                                  TrajOptProbPtr prob)
+Optimizer::Callback WriteCallback(shared_ptr<ofstream> file, TrajOptProbPtr prob)
 {
   if (!file->good())
   {
