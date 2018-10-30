3c79ef8db63155cde646b91e42765633fc0a6b96
Matthew Powelson <matthew.powelson@swri.org>
2018-10-30 08:31:28 -0500

    File Write Callback: Update License and minor fixes

--- trajopt/src/file_write_callback.cpp
+++ trajopt/src/file_write_callback.cpp
@@ -1,29 +1,18 @@
 /*
-Copyright 2018, Southwest Research Institute
+Copyright 2018 Southwest Research Institute
 
-    Redistribution and use in source and binary forms, with or without modification,
-    are permitted provided that the following conditions are met :
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    1. Redistributions of source code must retain the above copyright notice,
-    this list of conditions and the following disclaimer.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-    2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation and / or
-        other materials provided with the distribution.
-
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-    INCLUDING, BUT NOT LIMITED TO,
-    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.IN
-        NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-    INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-    OR CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-                             LOSS OF USE, DATA, OR PROFITS;
-                             OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-    WHETHER IN CONTRACT, STRICT LIABILITY,
-    OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 #include <functional>
 #include <fstream>
 #include <trajopt/file_write_callback.hpp>
@@ -77,14 +66,14 @@ void WriteFile(shared_ptr<ofstream> file,
 
     // Write costs to file
     std::vector<double> costs = results.cost_vals;
-    for (auto cost : costs)
+    for (const auto& cost : costs)
     {
       *file << ',' << cost;
     }
 
     // Write constraints to file
     std::vector<double> constraints = results.cnt_viols;
-    for (auto constraint : constraints)
+    for (const auto& constraint : constraints)
     {
       *file << ',' << constraint;
     }
@@ -121,14 +110,14 @@ Optimizer::Callback WriteCallback(shared_ptr<ofstream> file, TrajOptProbPtr prob
 
   // Write cost names
   vector<CostPtr> costs = prob->getCosts();
-  for (auto cost : costs)
+  for (const auto& cost : costs)
   {
     *file << ',' << cost->name();
   }
 
   // Write constraint names
   vector<ConstraintPtr> cnts = prob->getConstraints();
-  for (auto cnt : cnts)
+  for (const auto& cnt : cnts)
   {
     *file << ',' << cnt->name();
   }
