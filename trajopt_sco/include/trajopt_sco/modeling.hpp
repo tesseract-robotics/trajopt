368e020389567f845840f50616974a666f753d84
Matthew Powelson <matthew.powelson@swri.org>
2018-11-07 11:23:13 -0600

    Add constructors to derived classes and rearranged for readability

--- trajopt_sco/include/trajopt_sco/modeling.hpp
+++ trajopt_sco/include/trajopt_sco/modeling.hpp
@@ -144,12 +144,17 @@ class EqConstraint : public Constraint
 {
 public:
   ConstraintType type() { return EQ; }
+  EqConstraint() : Constraint() {}
+  EqConstraint(const string& name) : Constraint(name) {}
+
 };
 
 class IneqConstraint : public Constraint
 {
 public:
   ConstraintType type() { return INEQ; }
+  IneqConstraint() : Constraint() {}
+  IneqConstraint(const string& name) : Constraint(name) {}
 };
 
 /**
