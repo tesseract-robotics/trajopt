064cffc2b1b68da87714344d383879a6abc958f1
Matthew Powelson <matthew.powelson@swri.org>
2018-11-07 11:24:02 -0600

    Add joint velocity constraint with tolerance

--- trajopt/include/trajopt/trajectory_costs.hpp
+++ trajopt/include/trajopt/trajectory_costs.hpp
@@ -48,6 +48,9 @@ private:
   sco::QuadExpr expr_;
   /** @brief Vector of velocity targets */
   VectorXd targs_;
+
+  // TODO: Add time steps
+  // TODO: Add getVars
 };
 
 /**
@@ -85,9 +88,72 @@ private:
   AffExpr expr_neg_;
 
   // TODO: Add time steps
+  // TODO: Add getVars
 };
 
-class TRAJOPT_API JointAccCost : public sco::Cost
+class TRAJOPT_API JointVelEqConstraint : public EqConstraint
+{
+public:
+  /** @brief Forms error in QuadExpr - independent of penalty type */
+  JointVelEqConstraint(const VarArray& traj, const VectorXd& coeffs, const VectorXd& targs);
+  /** @brief Convexifies cost expression - In this case, it is already quadratic so there's nothing to do */
+  virtual ConvexConstraintsPtr convex(const vector<double>& x, Model* model);
+  /** @brief Numerically evaluate cost given the vector of values */
+  virtual vector<double> value(const vector<double>&);
+  /** Calculate constraint violations (positive part for inequality constraint,
+   * absolute value for inequality constraint)*/
+  vector<double> violations(const vector<double>& x);
+  /** Sum of violations */
+  double violation(const vector<double>& x);
+
+private:
+  /** @brief The variables being optimized. Used to properly index the vector being optimized */
+  VarArray vars_;
+  /** @brief The coefficients used to weight the cost */
+  VectorXd coeffs_;
+  /** @brief Stores the cost as an expression */
+  AffExpr expr_;
+  /** @brief Vector of velocity targets */
+  VectorXd targs_;
+
+  // TODO: Add time steps
+};
+
+class TRAJOPT_API JointVelIneqConstraint : public IneqConstraint
+{
+public:
+  /** @brief Forms error in QuadExpr - independent of penalty type */
+  JointVelIneqConstraint(const VarArray& traj,
+                   const VectorXd& coeffs,
+                   const VectorXd& targs,
+                   const VectorXd& upper_limits,
+                   const VectorXd& lower_limits);
+  /** @brief Convexifies cost expression - In this case, it is already quadratic so there's nothing to do */
+  virtual ConvexConstraintsPtr convex(const vector<double>& x, Model* model);
+  /** @brief Numerically evaluate cost given the vector of values */
+  virtual vector<double> value(const vector<double>&);
+
+private:
+  /** @brief The variables being optimized. Used to properly index the vector being optimized */
+  VarArray vars_;
+  /** @brief The coefficients used to weight the cost */
+  VectorXd coeffs_;
+  /** @brief Vector of velocity targets */
+  VectorXd upper_tols_;
+  /** @brief Vector of velocity targets */
+  VectorXd lower_tols_;
+  /** @brief Vector of velocity targets */
+  VectorXd targs_;
+  /** @brief Stores the cost as an expression */
+  AffExpr expr_;
+  /** @brief Stores the cost as an expression */
+  AffExpr expr_neg_;
+
+  // TODO: Add time steps
+  // TODO: Add getVars
+};
+
+class TRAJOPT_API JointAccCost : public Cost
 {
 public:
   JointAccCost(const VarArray& traj, const Eigen::VectorXd& coeffs);
