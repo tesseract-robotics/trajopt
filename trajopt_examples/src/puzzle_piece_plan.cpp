e40f4abfd5fdceed5f8d2e73e3fc46643015ccfa
Matthew Powelson <matthew.powelson@swri.org>
2019-02-08 08:49:22 -0600

    Fix Joint Term Default Values
    Time interval defaults to the whole problem. Updated the docs to state that coeffs has a default value, but targets is required. Also updated the examples to add the time steps to them.

--- trajopt_examples/src/puzzle_piece_plan.cpp
+++ trajopt_examples/src/puzzle_piece_plan.cpp
@@ -142,6 +142,8 @@ ProblemConstructionInfo cppMethod()
   std::shared_ptr<JointVelTermInfo> joint_vel = std::shared_ptr<JointVelTermInfo>(new JointVelTermInfo);
   joint_vel->coeffs = std::vector<double>(7, 1.0);
   joint_vel->targets = std::vector<double>(7, 0.0);
+  joint_vel->first_step = 0;
+  joint_vel->last_step = pci.basic_info.n_steps - 1;
   joint_vel->name = "joint_vel";
   joint_vel->term_type = TT_COST;
   pci.cost_infos.push_back(joint_vel);
@@ -149,6 +151,8 @@ ProblemConstructionInfo cppMethod()
   std::shared_ptr<JointAccTermInfo> joint_acc = std::shared_ptr<JointAccTermInfo>(new JointAccTermInfo);
   joint_acc->coeffs = std::vector<double>(7, 2.0);
   joint_acc->targets = std::vector<double>(7, 0.0);
+  joint_acc->first_step = 0;
+  joint_acc->last_step = pci.basic_info.n_steps - 1;
   joint_acc->name = "joint_acc";
   joint_acc->term_type = TT_COST;
   pci.cost_infos.push_back(joint_acc);
@@ -156,6 +160,8 @@ ProblemConstructionInfo cppMethod()
   std::shared_ptr<JointJerkTermInfo> joint_jerk = std::shared_ptr<JointJerkTermInfo>(new JointJerkTermInfo);
   joint_jerk->coeffs = std::vector<double>(7, 5.0);
   joint_jerk->targets = std::vector<double>(7, 0.0);
+  joint_jerk->first_step = 0;
+  joint_jerk->last_step = pci.basic_info.n_steps - 1;
   joint_jerk->name = "joint_jerk";
   joint_jerk->term_type = TT_COST;
   pci.cost_infos.push_back(joint_jerk);
