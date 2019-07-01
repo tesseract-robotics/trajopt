1797c6094ad1e091a89d4b4228acc98108270be2
Levi Armstrong <levi.armstrong@gmail.com>
2019-07-01 16:48:16 -0500

    Update unit tests

--- trajopt_sco/test/solver-interface-unit.cpp
+++ trajopt_sco/test/solver-interface-unit.cpp
@@ -241,4 +241,8 @@ TEST_P(SolverInterface, ExprMult_test3)
   EXPECT_NEAR(aff12.value(soln), answer, 1e-6);
 }
 
-INSTANTIATE_TEST_CASE_P(AllSolvers, SolverInterface, testing::ValuesIn(availableSolvers()));
+auto getAvailableSolvers = [] () { std::vector<ModelType> solvers = availableSolvers();
+  auto it = std::find(solvers.begin(), solvers.end(),ModelType::OSQP);
+  if(it != solvers.end()) { solvers.erase(it); }; return solvers; };
+
+INSTANTIATE_TEST_CASE_P(AllSolvers, SolverInterface, testing::ValuesIn(getAvailableSolvers()));
