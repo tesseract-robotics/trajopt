45dd9592e4d7cda6bab0c1464f200b64c0deac13
Michael Ripperger <michael.ripperger@swri.org>
2020-03-03 16:43:58 -0600

    Fixed bug in collision interpolation step

--- trajopt/src/collision_terms.cpp
+++ trajopt/src/collision_terms.cpp
@@ -879,32 +879,22 @@ void CollisionEvaluator::processInterpolatedCollisionResults(
       // Update cc_time and cc_type
       for (auto& r : pair.second)
       {
-        if (std::find(active_links.begin(), active_links.end(), r.link_names[0]) != active_links.end())
+        // Iterate over the two time values in r.cc_time
+        for (size_t j = 0; j < 2; ++j)
         {
-          r.cc_time[0] =
-              (r.cc_time[0] < 0) ? (static_cast<double>(i) * dt) : (static_cast<double>(i) * dt) + (r.cc_time[0] * dt);
-          assert(r.cc_time[0] >= 0.0 && r.cc_time[0] <= 1.0);
-          if (i == 0 && r.cc_type[0] == tesseract_collision::ContinuousCollisionType::CCType_Time0)
-            r.cc_type[0] = tesseract_collision::ContinuousCollisionType::CCType_Time0;
-          else if (i == (contacts_vector.size() - 1) &&
-                   r.cc_type[0] == tesseract_collision::ContinuousCollisionType::CCType_Time1)
-            r.cc_type[0] = tesseract_collision::ContinuousCollisionType::CCType_Time1;
-          else
-            r.cc_type[0] = tesseract_collision::ContinuousCollisionType::CCType_Between;
-        }
-
-        if (std::find(active_links.begin(), active_links.end(), r.link_names[1]) != active_links.end())
-        {
-          r.cc_time[1] =
-              (r.cc_time[0] < 0) ? (static_cast<double>(i) * dt) : (static_cast<double>(i) * dt) + (r.cc_time[1] * dt);
-          assert(r.cc_time[1] >= 0.0 && r.cc_time[1] <= 1.0);
-          if (i == 0 && r.cc_type[1] == tesseract_collision::ContinuousCollisionType::CCType_Time0)
-            r.cc_type[1] = tesseract_collision::ContinuousCollisionType::CCType_Time0;
-          else if (i == (contacts_vector.size() - 1) &&
-                   r.cc_type[1] == tesseract_collision::ContinuousCollisionType::CCType_Time1)
-            r.cc_type[1] = tesseract_collision::ContinuousCollisionType::CCType_Time1;
-          else
-            r.cc_type[1] = tesseract_collision::ContinuousCollisionType::CCType_Between;
+          if (std::find(active_links.begin(), active_links.end(), r.link_names[j]) != active_links.end())
+          {
+            r.cc_time[j] = (r.cc_time[j] < 0) ? (static_cast<double>(i) * dt) :
+                                                (static_cast<double>(i) * dt) + (r.cc_time[j] * dt);
+            assert(r.cc_time[j] >= 0.0 && r.cc_time[j] <= 1.0);
+            if (i == 0 && r.cc_type[j] == tesseract_collision::ContinuousCollisionType::CCType_Time0)
+              r.cc_type[j] = tesseract_collision::ContinuousCollisionType::CCType_Time0;
+            else if (i == (contacts_vector.size() - 1) &&
+                     r.cc_type[j] == tesseract_collision::ContinuousCollisionType::CCType_Time1)
+              r.cc_type[j] = tesseract_collision::ContinuousCollisionType::CCType_Time1;
+            else
+              r.cc_type[j] = tesseract_collision::ContinuousCollisionType::CCType_Between;
+          }
         }
       }
 
