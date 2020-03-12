b8306776aa6a17b6d479363fa5acea3bd8a4bbdd
Joe Schornak <joe.schornak@gmail.com>
2020-03-11 19:52:40 -0500

    Update FindGUROBI.cmake to match newer example

--- trajopt_sco/cmake/FindGUROBI.cmake
+++ trajopt_sco/cmake/FindGUROBI.cmake
@@ -4,40 +4,32 @@
 # environment variables:
 # GUROBI_HOME
 # 
-# e.g. GUROBI_HOME=/opt/gurobobi810/linux64
+# e.g. GUROBI_HOME=/opt/gurobi901/linux64
 #
 # The module will define:
 # GUROBI_FOUND - If we found Gurobi in the system
-# GUROBI_INCLUDE_DIR - Gurobi include directories
+# GUROBI_INCLUDE_DIRS - Gurobi include directories
 # GUROBI_LIBRARIES - libraries needed to use Gurobi
 #
 # This module will NOT enforce a specific GUROBI version, rather it will
-# only work with latest GUROBI version (8.1.0)
+# only work with latest GUROBI versions (8.1.0 and 9.0)
 
-find_path(GUROBI_INCLUDE_DIR NAMES gurobi_c++.h
-          HINTS
-          ENV GUROBI_HOME
-          PATH_SUFFIXES include
-          )
+find_path(GUROBI_INCLUDE_DIRS
+    NAMES gurobi_c.h
+    HINTS ${GUROBI_DIR} $ENV{GUROBI_HOME}
+    PATH_SUFFIXES include)
 
-find_library(GUROBI_LIBRARY NAMES gurobi81
-             HINTS
-             ENV GUROBI_HOME
-             PATH_SUFFIXES lib
-             )
+find_library(GUROBI_LIBRARY
+    NAMES gurobi gurobi81 gurobi90
+    HINTS ${GUROBI_DIR} $ENV{GUROBI_HOME}
+    PATH_SUFFIXES lib)
 
-find_library(GUROBI_CXX_LIBRARY NAMES gurobi_c++
-             HINTS
-             ENV GUROBI_HOME
-             PATH_SUFFIXES lib
-             )
-
-set(GUROBI_LIBRARIES  ${GUROBI_LIBRARY} ${GUROBI_CXX_LIBRARY})
+set(GUROBI_LIBRARIES ${GUROBI_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GUROBI_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(GUROBI  DEFAULT_MSG
-                                  GUROBI_LIBRARY GUROBI_CXX_LIBRARY GUROBI_INCLUDE_DIR)
+                                  GUROBI_LIBRARY GUROBI_INCLUDE_DIRS)
 
-mark_as_advanced(GUROBI_INCLUDE_DIR GUROBI_LIBRARY GUROBI_CXX_LIBRARY GUROBI_LIBRARIES)
+mark_as_advanced(GUROBI_INCLUDE_DIRS GUROBI_LIBRARY GUROBI_LIBRARIES)
