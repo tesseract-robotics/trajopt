9e8fe8cb3a240966ed82bf330b1a882aed9ad5d0
Matthew Powelson <powelson.matthew@gmail.com>
2020-05-18 15:39:08 -0500

    trajopt_ifopt/trajopt_sqp: Add Apache 2 license notices

--- trajopt_optimizers/trajopt_sqp/src/trust_region_sqp_solver.cpp
+++ trajopt_optimizers/trajopt_sqp/src/trust_region_sqp_solver.cpp
@@ -1,3 +1,33 @@
+/**
+ * @file trust_region_sqp_solver.cpp
+ * @brief Contains the main trust region SQP solver. While it is based on the paper below, it has been completely
+ * rewritten from trajopt_sco
+ *
+ * Schulman, J., Ho, J., Lee, A. X., Awwal, I., Bradlow, H., & Abbeel, P. (2013, June). Finding Locally Optimal,
+ * Collision-Free Trajectories with Sequential Convex Optimization. In Robotics: science and systems (Vol. 9, No. 1, pp.
+ * 1-10).
+ *
+ * @author Matthew Powelson
+ * @date May 18, 2020
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2020, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include <trajopt_sqp/trust_region_sqp_solver.h>
 #include <iostream>
 #include <console_bridge/console.h>
