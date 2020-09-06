62917d716b391012e2ea807d52562f72c3f8b9ed
Matthew Powelson <powelson.matthew@gmail.com>
2020-09-06 22:10:58 +0100

    Add trajopt_ifopt.h

---
+++ trajopt_ifopt/include/trajopt_ifopt/trajopt_ifopt.h
@@ -0,0 +1,41 @@
+/**
+ * @file trajopt_ifopt.h
+ * @brief Includes all of the trajopt_ifopt headers
+ *
+ * @author Matthew Powelson
+ * @date September 8, 2020
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef TRAJOPT_IFOPT_TRAJOPT_IFOPT_H
+#define TRAJOPT_IFOPT_TRAJOPT_IFOPT_H
+
+#include <trajopt_ifopt/constraints/cartesian_position_constraint.h>
+#include <trajopt_ifopt/constraints/collision_constraint.h>
+#include <trajopt_ifopt/constraints/inverse_kinematics_constraint.h>
+#include <trajopt_ifopt/constraints/joint_position_constraint.h>
+#include <trajopt_ifopt/constraints/joint_velocity_constraint.h>
+
+#include <trajopt_ifopt/costs/squared_cost.h>
+
+#include <trajopt_ifopt/utils/ifopt_utils.h>
+#include <trajopt_ifopt/utils/numeric_differentiation.h>
+#include <trajopt_ifopt/utils/trajopt_utils.h>
+
+#include <trajopt_ifopt/variable_sets/joint_position_variable.h>
+
+#endif
