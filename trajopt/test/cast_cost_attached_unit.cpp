93013412641ab08bd06cd23a4cea10af64a1183d
Levi Armstrong <levi.armstrong@gmail.com>
2020-09-09 14:44:36 -0500

    Fix warnings and update to use tesseract Manipulator Manager

--- trajopt/test/cast_cost_attached_unit.cpp
+++ trajopt/test/cast_cost_attached_unit.cpp
@@ -68,6 +68,7 @@ public:
     box_attached_link.collision.push_back(collision);
 
     Joint box_attached_joint("boxbot_link-box_attached");
+    box_attached_joint.type = JointType::FIXED;
     box_attached_joint.parent_link_name = "boxbot_link";
     box_attached_joint.child_link_name = "box_attached";
     box_attached_joint.parent_to_joint_origin_transform.translation() = Eigen::Vector3d(0.5, -0.5, 0);
@@ -81,6 +82,7 @@ public:
     box_attached2_link.collision.push_back(collision);
 
     Joint box_attached2_joint("no_geom_link-box_attached2");
+    box_attached2_joint.type = JointType::FIXED;
     box_attached2_joint.parent_link_name = "no_geom_link";
     box_attached2_joint.child_link_name = "box_attached2";
     box_attached2_joint.parent_to_joint_origin_transform.translation() = Eigen::Vector3d(0, 0, 0);
