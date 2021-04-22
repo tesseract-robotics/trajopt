8966bea8e1df293a01d0684b790a2d2d96d7141e
ben <ben.greenberg@swri.org>
2021-04-22 14:22:13 -0400

    add joint acceleration constraint

---
+++ trajopt_ifopt/include/trajopt_ifopt/constraints/joint_acceleration_constraint.h
@@ -0,0 +1,106 @@
+/**
+ * @file joint_acceleration_constraint.h
+ * @brief The joint_acceleration constraint
+ *
+ * @author Ben Greenberg
+ * @date April 22, 2021
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2021, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef TRAJOPT_IFOPT_JOINT_JOINT_ACCELERATION_CONSTRAINT_H
+#define TRAJOPT_IFOPT_JOINT_JOINT_ACCELERATION_CONSTRAINT_H
+
+#include <trajopt_utils/macros.h>
+TRAJOPT_IGNORE_WARNINGS_PUSH
+#include <ifopt/constraint_set.h>
+#include <Eigen/Eigen>
+TRAJOPT_IGNORE_WARNINGS_POP
+
+#include <trajopt_ifopt/variable_sets/joint_velocity_variable.h>
+
+namespace trajopt
+{
+/**
+ * @brief This creates a joint acceleration constraint and allows bounds to be set on a joint position
+ *
+ * Joint acceleration is calculated as a = th_1 - th_0
+ */
+class JointAccelConstraint : public ifopt::ConstraintSet
+{
+public:
+  using Ptr = std::shared_ptr<JointAccelConstraint>;
+  using ConstPtr = std::shared_ptr<const JointAccelConstraint>;
+
+  /**
+   * @brief Constructs a acceleration contraint from these variables, setting the bounds to the target
+   * @param targets Joint Acceleration targets (length should be n_dof). Upper and lower bounds are set to this value
+   * @param position_vars Joint velocities used to calculate acceleration. These vars are assumed to be continuous and in
+   * order.
+   * @param name Name of the constraint
+   */
+  JointAccelConstraint(const Eigen::VectorXd& targets,
+                     std::vector<JointVelocity::ConstPtr> position_vars,
+                     const std::string& name = "JointAccel");
+
+  /**
+   * @brief Constructs a acceleration contraint from these variables, setting the bounds to those passed in.
+   * @param bounds Bounds on target joint acceleration (length should be n_dof)
+   * @param position_vars Joint positions used to calculate acceleration. These vars are assumed to be continuous and in
+   * order.
+   * @param name Name of the constraint
+   */
+  JointAccelConstraint(const std::vector<ifopt::Bounds>& bounds,
+                     std::vector<JointVelocity::ConstPtr> position_vars,
+                     const std::string& name = "JointAccel");
+
+  /**
+   * @brief Returns the values associated with the constraint. In this case that is the approximate joint acceleration.
+   * @return Returns jointAcceleration. Length is n_dof_ * (n_vars - 1)
+   */
+  Eigen::VectorXd GetValues() const override;
+
+  /**
+   * @brief  Returns the "bounds" of this constraint. How these are enforced is up to the solver
+   * @return Returns the "bounds" of this constraint
+   */
+  std::vector<ifopt::Bounds> GetBounds() const override;
+
+  /**
+   * @brief Fills the jacobian block associated with the given var_set.
+   * @param var_set Name of the var_set to which the jac_block is associated
+   * @param jac_block Block of the overal jacobian associated with these constraints and the var_set variable
+   */
+  void FillJacobianBlock(std::string var_set, Jacobian& jac_block) const override;
+
+private:
+  /** @brief The number of joints in a single JointVelocity */
+  long n_dof_;
+  /** @brief The number of JointVelocitys passed in */
+  long n_vars_;
+
+  /** @brief Bounds on the velocities of each joint */
+  std::vector<ifopt::Bounds> bounds_;
+
+  /** @brief Pointers to the vars used by this constraint.
+   *
+   * Do not access them directly. Instead use this->GetVariables()->GetComponent(position_var->GetName())->GetValues()*/
+  std::vector<JointVelocity::ConstPtr> velocity_vars_;
+};
+};  // namespace trajopt
+#endif
---
+++ trajopt_ifopt/src/joint_acceleration_constraint.cpp
@@ -0,0 +1,108 @@
+/**
+ * @file joint_acceleration_constraint.h
+ * @brief The joint_acceleration constraint
+ *
+ * @author Ben Greenberg
+ * @date April 22, 2021
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2021, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <trajopt_ifopt/constraints/joint_acceleration_constraint.h>
+
+TRAJOPT_IGNORE_WARNINGS_PUSH
+#include <console_bridge/console.h>
+TRAJOPT_IGNORE_WARNINGS_POP
+
+namespace trajopt
+{
+JointAccelConstraint::JointAccelConstraint(const Eigen::VectorXd& targets,
+                                       std::vector<JointVelocity::ConstPtr> velocity_vars,
+                                       const std::string& name)
+  : ifopt::ConstraintSet(static_cast<int>(targets.size()) * static_cast<int>(velocity_vars.size() - 1), name)
+  , velocity_vars_(velocity_vars)
+{
+  // Check and make sure the targets size aligns with the vars passed in
+  for (auto& velocity_var : velocity_vars)
+  {
+    if (targets.size() != velocity_var->GetRows())
+      CONSOLE_BRIDGE_logError("Targets size does not align with variables provided");
+  }
+
+  // Set n_dof and n_vars
+  n_dof_ = targets.size();
+  n_vars_ = static_cast<long>(velocity_vars.size());
+  assert(n_dof_ > 0);
+  assert(n_vars_ > 0);
+  //  assert(n_vars_ == 2);
+
+  // Set the bounds to the input targets
+  std::vector<ifopt::Bounds> bounds(static_cast<size_t>(GetRows()));
+  // All of the positions should be exactly at their targets
+  for (long j = 0; j < n_vars_ - 1; j++)
+  {
+    for (long i = 0; i < n_dof_; i++)
+    {
+      bounds[static_cast<size_t>(i + j * n_dof_)] = ifopt::Bounds(targets[i], targets[i]);
+    }
+  }
+  bounds_ = bounds;
+}
+
+Eigen::VectorXd JointAccelConstraint::GetValues() const
+{
+  Eigen::VectorXd acceleration(static_cast<size_t>(n_dof_) * (velocity_vars_.size() - 1));
+  for (std::size_t ind = 0; ind < velocity_vars_.size() - 1; ind++)
+  {
+    auto vals1 = GetVariables()->GetComponent(velocity_vars_[ind]->GetName())->GetValues();
+    auto vals2 = GetVariables()->GetComponent(velocity_vars_[ind + 1]->GetName())->GetValues();
+    Eigen::VectorXd single_step = vals1 - vals2;
+    acceleration.block(n_dof_ * static_cast<Eigen::Index>(ind), 0, n_dof_, 1) = single_step;
+  }
+
+  return acceleration;
+}
+
+// Set the limits on the constraint values (in this case just the targets)
+std::vector<ifopt::Bounds> JointAccelConstraint::GetBounds() const { return bounds_; }
+
+void JointAccelConstraint::FillJacobianBlock(std::string var_set, Jacobian& jac_block) const
+{
+  // Loop over all of the variables this constraint uses
+  for (int i = 0; i < n_vars_; i++)
+  {
+    // Only modify the jacobian if this constraint uses var_set
+    if (var_set == velocity_vars_[static_cast<size_t>(i)]->GetName())
+    {
+      // Reserve enough room in the sparse matrix
+      jac_block.reserve(n_dof_ * 2);
+
+      // jac block will be (n_vars-1)*n_dof x n_dof
+      for (int j = 0; j < n_dof_; j++)
+      {
+        // The first and last variable are special and only effect the first and last constraint. Everything else
+        // effects 2
+        if (i < n_vars_ - 1)
+          jac_block.coeffRef(i * n_dof_ + j, j) = 1.0;
+        if (i > 0)
+          jac_block.coeffRef((i - 1) * n_dof_ + j, j) = -1.0;
+      }
+    }
+  }
+}
+}  // namespace trajopt
