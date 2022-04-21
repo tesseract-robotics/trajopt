59610f8b0b2fb728256ab86f52674978eb4c78f8
John Wason <wason@wasontech.com>
2022-04-21 00:25:23 -0400

    cmake format

--- trajopt_ext/osqp/patch_osqp.cmake
+++ trajopt_ext/osqp/patch_osqp.cmake
@@ -1,7 +1,15 @@
 file(READ CMakeLists.txt ROOT_FILE)
-string(REPLACE "set_target_properties(osqpstatic PROPERTIES OUTPUT_NAME osqp)" "" ROOT_FILE2 ${ROOT_FILE})
+string(
+  REPLACE "set_target_properties(osqpstatic PROPERTIES OUTPUT_NAME osqp)"
+          ""
+          ROOT_FILE2
+          ${ROOT_FILE})
 file(WRITE CMakeLists.txt ${ROOT_FILE2})
 
 file(READ lin_sys/direct/qdldl/qdldl_sources/CMakeLists.txt QDLDL_FILE)
-string(REPLACE "set_target_properties(qdldlstatic PROPERTIES OUTPUT_NAME qdldl)" "" QDLDL_FILE2 ${QDLDL_FILE})
+string(
+  REPLACE "set_target_properties(qdldlstatic PROPERTIES OUTPUT_NAME qdldl)"
+          ""
+          QDLDL_FILE2
+          ${QDLDL_FILE})
 file(WRITE lin_sys/direct/qdldl/qdldl_sources/CMakeLists.txt ${QDLDL_FILE2})
