3c2d7268631818b7b5d749de065402376b392abd
John Wason <wason@wasontech.com>
2023-06-03 14:52:45 -0400

    Add patch to build shared qpoases, only build qpoases on Windows

---
+++ trajopt_ext/qpoases/patch_qpoases.cmake
@@ -0,0 +1,8 @@
+file(READ CMakeLists.txt ROOT_FILE)
+string(
+  REPLACE "MESSAGE(FATAL_ERROR \"Compiling qpOASES as a shared library in Windows is not supported.\")"
+          "set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)\ninclude(GNUInstallDirs)"
+          ROOT_FILE2
+          ${ROOT_FILE})
+  string(REPLACE "SET(CMAKE_INSTALL_BINDIR \${CMAKE_INSTALL_LIBDIR})" "" ROOT_FILE3 ${ROOT_FILE2})
+file(WRITE CMakeLists.txt ${ROOT_FILE3})
