faf7f5bf34fc021b64871ba1a0cbccf941bbe157
Levi Armstrong <levi.armstrong@gmail.com>
2023-07-31 16:03:32 -0500

    Fix unit tests

--- trajopt_optimizers/trajopt_sqp/test/cast_cost_unit.cpp
+++ trajopt_optimizers/trajopt_sqp/test/cast_cost_unit.cpp
@@ -115,6 +115,7 @@ void runCastTest(const trajopt_sqp::QPProblem::Ptr& qp_problem, const Environmen
   double margin_coeff = 10;
   double margin = 0.02;
   auto trajopt_collision_config = std::make_shared<trajopt_ifopt::TrajOptCollisionConfig>(margin, margin_coeff);
+  trajopt_collision_config->type = tesseract_collision::CollisionEvaluatorType::LVS_CONTINUOUS;
   trajopt_collision_config->collision_margin_buffer = 0.05;
 
   // 4) Add constraints
