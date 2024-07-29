692196f21c326e462fbfdfb4586764038f79df55
John Wason <wason@wasontech.com>
2024-07-29 00:38:27 -0400

    Fix libqpOASES.3.2.dylib rpath nightmare on mac os

--- trajopt_ext/qpoases/patch_qpoases.cmake
+++ trajopt_ext/qpoases/patch_qpoases.cmake
@@ -1,12 +1,12 @@
 file(READ CMakeLists.txt ROOT_FILE)
 string(
   REPLACE "MESSAGE(FATAL_ERROR \"Compiling qpOASES as a shared library in Windows is not supported.\")"
-          "set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)\ninclude(GNUInstallDirs)"
+          "set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)\nENDIF()\ninclude(GNUInstallDirs)\nIF(0)\n"
           ROOT_FILE2
           ${ROOT_FILE})
-string(
-  REPLACE "SET(CMAKE_INSTALL_BINDIR \${CMAKE_INSTALL_LIBDIR})"
-          ""
-          ROOT_FILE3
-          ${ROOT_FILE2})
-file(WRITE CMakeLists.txt ${ROOT_FILE3})
+string(REGEX REPLACE "SET\\(CMAKE_INSTALL[^\r\n]*\\)" "" ROOT_FILE3 ${ROOT_FILE2})
+string(REGEX REPLACE "set\\([^\r\n]*RPATH[^\r\n]*\\)" "" ROOT_FILE4 ${ROOT_FILE3})
+string(REGEX REPLACE "SET\\([^\r\n]*OUTPUT_PATH[^\r\n]*\\)" "" ROOT_FILE5 ${ROOT_FILE4})
+string(REPLACE "cmake_minimum_required(VERSION 2.6)" "cmake_minimum_required(VERSION 3.10.0)" ROOT_FILE6 ${ROOT_FILE5}) 
+
+file(WRITE CMakeLists.txt ${ROOT_FILE6})
