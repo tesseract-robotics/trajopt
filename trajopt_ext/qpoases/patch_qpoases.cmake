1815a0889320569461bec3a035486ffad164110e
John Wason <wason@wasontech.com>
2024-07-30 14:54:24 -0400

    code format

--- trajopt_ext/qpoases/patch_qpoases.cmake
+++ trajopt_ext/qpoases/patch_qpoases.cmake
@@ -4,9 +4,28 @@ string(
           "set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)\nENDIF()\ninclude(GNUInstallDirs)\nIF(0)\n"
           ROOT_FILE2
           ${ROOT_FILE})
-string(REGEX REPLACE "SET\\(CMAKE_INSTALL[^\r\n]*\\)" "" ROOT_FILE3 ${ROOT_FILE2})
-string(REGEX REPLACE "set\\([^\r\n]*RPATH[^\r\n]*\\)" "" ROOT_FILE4 ${ROOT_FILE3})
-string(REGEX REPLACE "SET\\([^\r\n]*OUTPUT_PATH[^\r\n]*\\)" "" ROOT_FILE5 ${ROOT_FILE4})
-string(REPLACE "cmake_minimum_required(VERSION 2.6)" "cmake_minimum_required(VERSION 3.10.0)" ROOT_FILE6 ${ROOT_FILE5}) 
+string(
+  REGEX
+  REPLACE "SET\\(CMAKE_INSTALL[^\r\n]*\\)"
+          ""
+          ROOT_FILE3
+          ${ROOT_FILE2})
+string(
+  REGEX
+  REPLACE "set\\([^\r\n]*RPATH[^\r\n]*\\)"
+          ""
+          ROOT_FILE4
+          ${ROOT_FILE3})
+string(
+  REGEX
+  REPLACE "SET\\([^\r\n]*OUTPUT_PATH[^\r\n]*\\)"
+          ""
+          ROOT_FILE5
+          ${ROOT_FILE4})
+string(
+  REPLACE "cmake_minimum_required(VERSION 2.6)"
+          "cmake_minimum_required(VERSION 3.10.0)"
+          ROOT_FILE6
+          ${ROOT_FILE5})
 
 file(WRITE CMakeLists.txt ${ROOT_FILE6})
