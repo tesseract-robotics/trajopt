5c6740d982b77908dd130f300ca0f8d260fdfb3e
Levi Armstrong <levi.armstrong@gmail.com>
2025-12-02 12:25:37 -0600

    Working joint velocity constraint using new variable set

--- trajopt_ifopt/include/trajopt_ifopt/variable_sets/var.h
+++ trajopt_ifopt/include/trajopt_ifopt/variable_sets/var.h
@@ -3,10 +3,10 @@
 
 #include <Eigen/Core>
 #include <vector>
-#include <memory>
 
 namespace trajopt_ifopt
 {
+class Node;
 /**
  * @brief This is the class which the constraints should be storing
  * @details This class contains all information necessary for filling the jacobian
@@ -17,12 +17,22 @@ class Var
 public:
   Var() = default;
   ~Var() = default;
-  Var(Eigen::Index index, std::string name);
-  Var(Eigen::Index index, Eigen::Index length, std::string identifier, std::vector<std::string> names);
+  Var(Eigen::Index index, std::string name, Node* parent = nullptr);
+  Var(Eigen::Index index,
+      Eigen::Index length,
+      std::string identifier,
+      std::vector<std::string> names,
+      Node* parent = nullptr);
   Var(const Var& other) = default;
   Var& operator=(const Var&) = default;
   Var(Var&&) = default;
   Var& operator=(Var&&) = default;
+
+  /** @brief Get the identifier for the variable */
+  const std::string& getIdentifier() const;
+
+  /** @brief Get the parent node */
+  const Node* getParent() const;
 
   /**
    * @brief Set the variables. This is the full vector of variables and it will extract its variables from the full
@@ -32,22 +42,28 @@ public:
   void setVariables(const Eigen::Ref<const Eigen::VectorXd>& x);
 
   /**
+   * @brief Get the index in the full set of variables that these are stored
+   * @return The start index
+   */
+  Eigen::Index getIndex() const;
+
+  /**
    * @brief Get the variable size
    * @return The size
    */
   Eigen::Index size() const;
 
-  template <typename T>
-  const T& value() const
-  {
-    throw std::runtime_error("This should never be used");
-  }
+  /**
+   * @brief Get variable values
+   * @return The values
+   */
+  const Eigen::VectorXd& value() const;
 
-  template <typename T>
-  const T& name() const
-  {
-    throw std::runtime_error("This should never be used");
-  }
+  /**
+   * @brief Get teh variable names
+   * @return The names
+   */
+  const std::vector<std::string>& name() const;
 
 private:
   friend class Node;
@@ -56,12 +72,7 @@ private:
   std::string identifier_;
   std::vector<std::string> names_;
   Eigen::VectorXd values_;
-
-  /**
-   * @brief Get the index in the full set of variables that these are stored
-   * @return The start index
-   */
-  Eigen::Index getIndex() const;
+  Node* parent_{ nullptr };
 
   /**
    * @brief Increment the start index by the provided value
@@ -70,37 +81,6 @@ private:
   void incrementIndex(Eigen::Index value);
 };
 
-template <>
-inline const double& Var::value() const
-{
-  assert(values_.size() == 1);
-  assert(index_ > -1);
-  assert(length_ == 1);
-  return values_[0];
-}
-
-template <>
-inline const Eigen::VectorXd& Var::value() const
-{
-  assert(index_ > -1);
-  assert(length_ > 1);
-  assert(names_.size() > 1);
-  return values_;
-}
-
-template <>
-inline const std::string& Var::name() const
-{
-  assert(names_.size() == 1);
-  return names_[0];
-}
-
-template <>
-inline const std::vector<std::string>& Var::name() const
-{
-  assert(names_.size() > 1);
-  return names_;
-}
 }  // namespace trajopt_ifopt
 
 #endif  // TRAJOPT_IFOPT_VAR_H
--- trajopt_ifopt/src/variable_sets/var.cpp
+++ trajopt_ifopt/src/variable_sets/var.cpp
@@ -2,28 +2,37 @@
 
 namespace trajopt_ifopt
 {
-Var::Var(Eigen::Index index, std::string name)
-  : index_(index), identifier_(std::move(name)), names_({ identifier_ }), values_(Eigen::VectorXd::Zero(1))
+Var::Var(Eigen::Index index, std::string name, Node* parent)
+  : index_(index)
+  , identifier_(std::move(name))
+  , names_({ identifier_ })
+  , values_(Eigen::VectorXd::Zero(1))
+  , parent_(parent)
 {
 }
 
-Var::Var(Eigen::Index index, Eigen::Index length, std::string identifier, std::vector<std::string> names)
+Var::Var(Eigen::Index index, Eigen::Index length, std::string identifier, std::vector<std::string> names, Node* parent)
   : index_(index)
   , length_(length)
   , identifier_(std::move(identifier))
   , names_(std::move(names))
-  , values_(Eigen::VectorXd::Zero(static_cast<Eigen::Index>(names.size())))
+  , values_(Eigen::VectorXd::Zero(static_cast<Eigen::Index>(names_.size())))
+  , parent_(parent)
 {
 }
 
+const std::string& Var::getIdentifier() const { return identifier_; }
+const Node* Var::getParent() const { return parent_; }
 Eigen::Index Var::getIndex() const { return index_; }
 Eigen::Index Var::size() const { return length_; }
+const Eigen::VectorXd& Var::value() const { return values_; }
+const std::vector<std::string>& Var::name() const { return names_; }
 
 void Var::incrementIndex(Eigen::Index value) { index_ += value; }
 void Var::setVariables(const Eigen::Ref<const Eigen::VectorXd>& x)
 {
   assert(index_ > -1 && index_ < x.size());
-  assert(length_ > 0 && (index_ + length_) < x.size());
+  assert(length_ > 0 && (index_ + (length_ - 1)) < x.size());
   values_ = x.segment(index_, length_);
 }
 
