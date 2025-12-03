ce4772aaba6ea9dde885037bc9e3cc98489b4f5a
Levi Armstrong <levi.armstrong@gmail.com>
2025-12-03 15:51:15 -0600

    Update collision and cartesian constraints to use new variable set

--- trajopt_ifopt/src/constraints/cartesian_position_constraint.cpp
+++ trajopt_ifopt/src/constraints/cartesian_position_constraint.cpp
@@ -24,6 +24,10 @@
  */
 #include <trajopt_ifopt/constraints/cartesian_position_constraint.h>
 #include <trajopt_ifopt/variable_sets/joint_position_variable.h>
+#include <trajopt_ifopt/variable_sets/nodes_variables.h>
+#include <trajopt_ifopt/variable_sets/node.h>
+#include <trajopt_ifopt/variable_sets/var.h>
+
 #include <trajopt_ifopt/utils/numeric_differentiation.h>
 #include <trajopt_ifopt/utils/trajopt_utils.h>
 #include <trajopt_common/utils.hpp>
@@ -354,6 +358,286 @@ Eigen::Isometry3d CartPosConstraint::GetCurrentPose() const
   transforms_cache.clear();
   const VectorXd joint_vals = this->GetVariables()->GetComponent(position_var_->GetName())->GetValues();
   info_.manip->calcFwdKin(transforms_cache, joint_vals);
+
+  return transforms_cache[info_.source_frame] * info_.source_frame_offset;
+}
+
+thread_local tesseract_common::TransformMap CartPosConstraint2::transforms_cache;  // NOLINT
+
+CartPosConstraint2::CartPosConstraint2(CartPosInfo info,
+                                       std::shared_ptr<const Var> position_var,
+                                       const Eigen::VectorXd& coeffs,  // NOLINT
+                                       const std::string& name)
+  : ifopt::ConstraintSet(static_cast<int>(info.indices.rows()), name)
+  , var_set_(position_var->getParent()->getParent()->GetName())
+  , coeffs_(coeffs)
+  , position_var_(std::move(position_var))
+  , info_(std::move(info))
+{
+  // Set the n_dof and n_vars for convenience
+  n_dof_ = info_.manip->numJoints();
+  assert(n_dof_ > 0);
+
+  bounds_ = std::vector<ifopt::Bounds>(static_cast<std::size_t>(info_.indices.rows()), ifopt::BoundZero);
+
+  if (coeffs_.rows() != info_.indices.rows())
+    std::runtime_error("The number of coeffs does not match the number of constraints.");
+
+  switch (info_.type)
+  {
+    case CartPosInfo::Type::TARGET_ACTIVE:
+    {
+      error_function_ = [this](const Eigen::Isometry3d& target_tf,
+                               const Eigen::Isometry3d& source_tf) -> Eigen::VectorXd {
+        Eigen::VectorXd err = tesseract_common::calcTransformError(source_tf, target_tf);
+
+        // This is available in 3.4 err(indices_, Eigen::all);
+        Eigen::VectorXd reduced_err(info_.indices.size());
+        for (int i = 0; i < info_.indices.size(); ++i)
+          reduced_err[i] = err[info_.indices[i]];
+
+        return reduced_err;
+      };
+
+      error_diff_function_ = [this](const Eigen::VectorXd& vals,
+                                    const Eigen::Isometry3d& target_tf,
+                                    const Eigen::Isometry3d& source_tf,
+                                    tesseract_common::TransformMap& transforms_cache) -> Eigen::VectorXd {
+        info_.manip->calcFwdKin(transforms_cache, vals);
+        const Eigen::Isometry3d perturbed_target_tf = transforms_cache[info_.target_frame] * info_.target_frame_offset;
+        Eigen::VectorXd error_diff =
+            tesseract_common::calcJacobianTransformErrorDiff(source_tf, target_tf, perturbed_target_tf);
+
+        // This is available in 3.4 err(indices_, Eigen::all);
+        Eigen::VectorXd reduced_error_diff(info_.indices.size());
+        for (int i = 0; i < info_.indices.size(); ++i)
+          reduced_error_diff[i] = error_diff[info_.indices[i]];
+
+        return reduced_error_diff;
+      };
+
+      break;
+    }
+    case CartPosInfo::Type::SOURCE_ACTIVE:
+    {
+      error_function_ = [this](const Eigen::Isometry3d& target_tf,
+                               const Eigen::Isometry3d& source_tf) -> Eigen::VectorXd {
+        Eigen::VectorXd err = tesseract_common::calcTransformError(target_tf, source_tf);
+
+        // This is available in 3.4 err(indices_, Eigen::all);
+        Eigen::VectorXd reduced_err(info_.indices.size());
+        for (int i = 0; i < info_.indices.size(); ++i)
+          reduced_err[i] = err[info_.indices[i]];
+
+        return reduced_err;
+      };
+
+      error_diff_function_ = [this](const Eigen::VectorXd& vals,
+                                    const Eigen::Isometry3d& target_tf,
+                                    const Eigen::Isometry3d& source_tf,
+                                    tesseract_common::TransformMap& transforms_cache) -> Eigen::VectorXd {
+        info_.manip->calcFwdKin(transforms_cache, vals);
+        const Eigen::Isometry3d perturbed_source_tf = transforms_cache[info_.source_frame] * info_.source_frame_offset;
+        Eigen::VectorXd error_diff =
+            tesseract_common::calcJacobianTransformErrorDiff(target_tf, source_tf, perturbed_source_tf);
+
+        // This is available in 3.4 err(indices_, Eigen::all);
+        Eigen::VectorXd reduced_error_diff(info_.indices.size());
+        for (int i = 0; i < info_.indices.size(); ++i)
+          reduced_error_diff[i] = error_diff[info_.indices[i]];
+
+        return reduced_error_diff;
+      };
+      break;
+    }
+    case CartPosInfo::Type::BOTH_ACTIVE:
+    {
+      error_function_ = [this](const Eigen::Isometry3d& target_tf,
+                               const Eigen::Isometry3d& source_tf) -> Eigen::VectorXd {
+        Eigen::VectorXd err = tesseract_common::calcTransformError(target_tf, source_tf);
+
+        // This is available in 3.4 err(indices_, Eigen::all);
+        Eigen::VectorXd reduced_err(info_.indices.size());
+        for (int i = 0; i < info_.indices.size(); ++i)
+          reduced_err[i] = err[info_.indices[i]];
+
+        return reduced_err;
+      };
+
+      error_diff_function_ = [this](const Eigen::VectorXd& vals,
+                                    const Eigen::Isometry3d& target_tf,
+                                    const Eigen::Isometry3d& source_tf,
+                                    tesseract_common::TransformMap& transforms_cache) -> Eigen::VectorXd {
+        info_.manip->calcFwdKin(transforms_cache, vals);
+        const Eigen::Isometry3d perturbed_source_tf = transforms_cache[info_.source_frame] * info_.source_frame_offset;
+        const Eigen::Isometry3d perturbed_target_tf = transforms_cache[info_.target_frame] * info_.target_frame_offset;
+        Eigen::VectorXd error_diff = tesseract_common::calcJacobianTransformErrorDiff(
+            target_tf, perturbed_target_tf, source_tf, perturbed_source_tf);
+
+        // This is available in 3.4 err(indices_, Eigen::all);
+        Eigen::VectorXd reduced_error_diff(info_.indices.size());
+        for (int i = 0; i < info_.indices.size(); ++i)
+          reduced_error_diff[i] = error_diff[info_.indices[i]];
+
+        return reduced_error_diff;
+      };
+      break;
+    }
+  }
+}
+
+CartPosConstraint2::CartPosConstraint2(const CartPosInfo& info,
+                                       std::shared_ptr<const Var> position_var,
+                                       const std::string& name)
+  : CartPosConstraint2(info, std::move(position_var), Eigen::VectorXd::Ones(info.indices.rows()), name)
+{
+}
+
+Eigen::VectorXd CartPosConstraint2::CalcValues(const Eigen::Ref<const Eigen::VectorXd>& joint_vals) const
+{
+  transforms_cache.clear();
+  info_.manip->calcFwdKin(transforms_cache, joint_vals);
+  const Eigen::Isometry3d source_tf = transforms_cache[info_.source_frame] * info_.source_frame_offset;
+  const Eigen::Isometry3d target_tf = transforms_cache[info_.target_frame] * info_.target_frame_offset;
+
+  const Eigen::VectorXd err = error_function_(target_tf, source_tf);
+
+  return coeffs_.cwiseProduct(err);
+}
+
+Eigen::VectorXd CartPosConstraint2::GetValues() const { return CalcValues(position_var_->value()); }
+
+// Set the limits on the constraint values
+std::vector<ifopt::Bounds> CartPosConstraint2::GetBounds() const { return bounds_; }
+
+void CartPosConstraint2::SetBounds(const std::vector<ifopt::Bounds>& bounds)
+{
+  assert(bounds.size() == 6);
+  bounds_ = bounds;
+}
+
+void CartPosConstraint2::CalcJacobianBlock(const Eigen::Ref<const Eigen::VectorXd>& joint_vals,
+                                           Jacobian& jac_block) const
+{
+  transforms_cache.clear();
+  info_.manip->calcFwdKin(transforms_cache, joint_vals);
+  const Eigen::Isometry3d source_tf = transforms_cache[info_.source_frame] * info_.source_frame_offset;
+  const Eigen::Isometry3d target_tf = transforms_cache[info_.target_frame] * info_.target_frame_offset;
+
+  std::vector<Eigen::Triplet<double> > triplet_list;
+  triplet_list.reserve(static_cast<std::size_t>(n_dof_ * info_.indices.size()));
+
+  constexpr double eps{ 1e-5 };
+  if (use_numeric_differentiation || info_.type == CartPosInfo::Type::BOTH_ACTIVE)
+  {
+    Eigen::MatrixXd jac0(info_.indices.size(), joint_vals.size());
+    Eigen::VectorXd dof_vals_pert = joint_vals;
+    for (int i = 0; i < joint_vals.size(); ++i)
+    {
+      dof_vals_pert(i) = joint_vals(i) + eps;
+      const VectorXd error_diff = error_diff_function_(dof_vals_pert, target_tf, source_tf, transforms_cache);
+      jac0.col(i) = error_diff / eps;
+      dof_vals_pert(i) = joint_vals(i);
+    }
+
+    for (int i = 0; i < info_.indices.size(); ++i)
+    {
+      for (int j = 0; j < n_dof_; j++)
+      {
+        // Each jac_block will be for a single variable but for all timesteps. Therefore we must index down to the
+        // correct timestep for this variable
+        triplet_list.emplace_back(i, j, coeffs_(i) * jac0(i, j));
+      }
+    }
+  }
+  else
+  {
+    // Paper:
+    // https://ethz.ch/content/dam/ethz/special-interest/mavt/robotics-n-intelligent-systems/rsl-dam/documents/RobotDynamics2016/RD2016script.pdf
+    // The jacobian of the robot is the geometric jacobian (Je) which maps generalized velocities in
+    // joint space to time derivatives of the end-effector configuration representation. It does not
+    // represent the analytic jacobian (Ja) given by a partial differentiation of position and rotation
+    // to generalized coordinates. Since the geometric jacobian is unique there exists a linear mapping
+    // between velocities and the derivatives of the representation.
+    //
+    // The approach in the paper was tried but it was having issues with getting correct jacobian.
+    // Must of had an error in the implementation so should revisit at another time but the approach
+    // below should be sufficient and faster than numerical calculations using the err function.
+
+    // The approach below leverages the geometric jacobian and a small step in time to approximate
+    // the partial derivative of the error function. Note that the rotational portion is the only part
+    // that is required to be modified per the paper.
+
+    // Calculate the jacobian
+    info_.manip->calcFwdKin(transforms_cache, joint_vals);
+    Eigen::MatrixXd jac0;
+    if (info_.type == CartPosInfo::Type::TARGET_ACTIVE)
+    {
+      jac0 = info_.manip->calcJacobian(joint_vals, info_.target_frame, info_.target_frame_offset.translation());
+      tesseract_common::jacobianChangeBase(
+          jac0, (transforms_cache[info_.source_frame] * info_.source_frame_offset).inverse());
+
+      for (int c = 0; c < jac0.cols(); ++c)
+      {
+        auto perturbed_target_tf = trajopt_common::addTwist(target_tf, jac0.col(c), eps);
+        const VectorXd error_diff =
+            tesseract_common::calcJacobianTransformErrorDiff(source_tf, target_tf, perturbed_target_tf);
+        jac0.col(c).tail(3) = (error_diff / eps);
+      }
+    }
+    else
+    {
+      jac0 = info_.manip->calcJacobian(joint_vals, info_.source_frame, info_.source_frame_offset.translation());
+      tesseract_common::jacobianChangeBase(
+          jac0, (transforms_cache[info_.target_frame] * info_.target_frame_offset).inverse());
+
+      for (int c = 0; c < jac0.cols(); ++c)
+      {
+        auto perturbed_source_tf = trajopt_common::addTwist(source_tf, jac0.col(c), eps);
+        const VectorXd error_diff =
+            tesseract_common::calcJacobianTransformErrorDiff(target_tf, source_tf, perturbed_source_tf);
+        jac0.col(c).tail(3) = (error_diff / eps);
+      }
+    }
+
+    // Convert to a sparse matrix and set the jacobian
+    for (int i = 0; i < info_.indices.size(); ++i)
+    {
+      for (int j = 0; j < n_dof_; j++)
+      {
+        // Each jac_block will be for a single variable but for all timesteps. Therefore we must index down to the
+        // correct timestep for this variable
+        triplet_list.emplace_back(i, j, coeffs_(i) * jac0(info_.indices[i], j));
+      }
+    }
+  }
+  jac_block.setFromTriplets(triplet_list.begin(), triplet_list.end());  // NOLINT
+}
+
+void CartPosConstraint2::FillJacobianBlock(std::string var_set, Jacobian& jac_block) const
+{
+  // Only modify the jacobian if this constraint uses var_set
+  if (var_set != var_set_)  // NOLINT
+    return;
+
+  // Get current joint values and calculate jacobian
+  CalcJacobianBlock(position_var_->value(), jac_block);  // NOLINT
+}
+
+const CartPosInfo& CartPosConstraint2::GetInfo() const { return info_; }
+CartPosInfo& CartPosConstraint2::GetInfo() { return info_; }
+
+void CartPosConstraint2::SetTargetPose(const Eigen::Isometry3d& target_frame_offset)
+{
+  info_.target_frame_offset = target_frame_offset;
+}
+
+Eigen::Isometry3d CartPosConstraint2::GetTargetPose() const { return info_.target_frame_offset; }
+
+Eigen::Isometry3d CartPosConstraint2::GetCurrentPose() const
+{
+  transforms_cache.clear();
+  info_.manip->calcFwdKin(transforms_cache, position_var_->value());
 
   return transforms_cache[info_.source_frame] * info_.source_frame_offset;
 }
--- trajopt_ifopt/src/constraints/collision/continuous_collision_constraint.cpp
+++ trajopt_ifopt/src/constraints/collision/continuous_collision_constraint.cpp
@@ -32,6 +32,9 @@ TRAJOPT_IGNORE_WARNINGS_POP
 #include <trajopt_ifopt/constraints/collision/continuous_collision_evaluators.h>
 #include <trajopt_ifopt/constraints/collision/weighted_average_methods.h>
 #include <trajopt_ifopt/variable_sets/joint_position_variable.h>
+#include <trajopt_ifopt/variable_sets/nodes_variables.h>
+#include <trajopt_ifopt/variable_sets/node.h>
+#include <trajopt_ifopt/variable_sets/var.h>
 
 namespace trajopt_ifopt
 {
@@ -197,6 +200,178 @@ void ContinuousCollisionConstraint::SetBounds(const std::vector<ifopt::Bounds>& 
 }
 
 std::shared_ptr<ContinuousCollisionEvaluator> ContinuousCollisionConstraint::GetCollisionEvaluator() const
+{
+  return collision_evaluator_;
+}
+
+ContinuousCollisionConstraint2::ContinuousCollisionConstraint2(
+    std::shared_ptr<ContinuousCollisionEvaluator> collision_evaluator,
+    std::array<std::shared_ptr<const Var>, 2> position_vars,
+    bool vars0_fixed,
+    bool vars1_fixed,
+    int max_num_cnt,
+    bool fixed_sparsity,
+    const std::string& name)
+  : ifopt::ConstraintSet(max_num_cnt, name)
+  , var_set_(position_vars.front()->getParent()->getParent()->GetName())
+  , position_vars_(std::move(position_vars))
+  , vars0_fixed_(vars0_fixed)
+  , vars1_fixed_(vars1_fixed)
+  , collision_evaluator_(std::move(collision_evaluator))
+{
+  if (position_vars_[0] == nullptr && position_vars_[1] == nullptr)
+    throw std::runtime_error("position_vars contains a nullptr!");
+
+  // Set n_dof_ for convenience
+  n_dof_ = position_vars_[0]->size();
+  if (!(n_dof_ > 0))
+    throw std::runtime_error("position_vars[0] is empty!");
+
+  if (position_vars_[0]->size() != position_vars_[1]->size())
+    throw std::runtime_error("position_vars are not the same size!");
+
+  if (vars0_fixed_ && vars1_fixed_)
+    throw std::runtime_error("position_vars are both fixed!");
+
+  if (max_num_cnt < 1)
+    throw std::runtime_error("max_num_cnt must be greater than zero!");
+
+  bounds_ = std::vector<ifopt::Bounds>(static_cast<std::size_t>(max_num_cnt), ifopt::BoundSmallerZero);
+
+  if (fixed_sparsity)
+  {
+    // Setting to zeros because snopt sparsity cannot change
+    triplet_list_.reserve(static_cast<std::size_t>(bounds_.size()) *
+                          static_cast<std::size_t>(position_vars_[0]->size()));
+
+    for (Eigen::Index i = 0; i < static_cast<Eigen::Index>(bounds_.size()); i++)  // NOLINT
+      for (Eigen::Index j = 0; j < n_dof_; j++)
+        triplet_list_.emplace_back(i, j, 0);
+  }
+}
+
+Eigen::VectorXd ContinuousCollisionConstraint2::GetValues() const
+{
+  // Get current joint values
+  // const Eigen::VectorXd joint_vals0 = this->GetVariables()->GetComponent(position_vars_[0]->GetName())->GetValues();
+  // const Eigen::VectorXd joint_vals1 = this->GetVariables()->GetComponent(position_vars_[1]->GetName())->GetValues();
+  const Eigen::VectorXd joint_vals0 = position_vars_[0]->value();
+  const Eigen::VectorXd joint_vals1 = position_vars_[1]->value();
+  const double margin_buffer = collision_evaluator_->GetCollisionMarginBuffer();
+  Eigen::VectorXd values = Eigen::VectorXd::Constant(static_cast<Eigen::Index>(bounds_.size()), -margin_buffer);
+
+  auto collision_data =
+      collision_evaluator_->CalcCollisionData(joint_vals0, joint_vals1, vars0_fixed_, vars1_fixed_, bounds_.size());
+
+  if (collision_data->gradient_results_sets.empty())
+    return values;
+
+  const std::size_t cnt = std::min(collision_data->gradient_results_sets.size(), bounds_.size());
+  if (!vars0_fixed_ && !vars1_fixed_)
+  {
+    for (std::size_t i = 0; i < cnt; ++i)
+    {
+      const trajopt_common::GradientResultsSet& r = collision_data->gradient_results_sets[i];
+      values(static_cast<Eigen::Index>(i)) = r.coeff * r.getMaxError();
+    }
+  }
+  else if (!vars0_fixed_)
+  {
+    for (std::size_t i = 0; i < cnt; ++i)
+    {
+      const trajopt_common::GradientResultsSet& r = collision_data->gradient_results_sets[i];
+      if (r.max_error[0].has_error[0] || r.max_error[1].has_error[0])
+        values(static_cast<Eigen::Index>(i)) = r.coeff * r.getMaxErrorT0();
+    }
+  }
+  else
+  {
+    for (std::size_t i = 0; i < cnt; ++i)
+    {
+      const trajopt_common::GradientResultsSet& r = collision_data->gradient_results_sets[i];
+      if (r.max_error[0].has_error[1] || r.max_error[1].has_error[1])
+        values(static_cast<Eigen::Index>(i)) = r.coeff * r.getMaxErrorT1();
+    }
+  }
+
+  return values;
+}
+
+// Set the limits on the constraint values
+std::vector<ifopt::Bounds> ContinuousCollisionConstraint2::GetBounds() const { return bounds_; }
+
+void ContinuousCollisionConstraint2::FillJacobianBlock(std::string var_set, Jacobian& jac_block) const
+{
+  // Only modify the jacobian if this constraint uses var_set
+  if (var_set != var_set_)  // NOLINT
+    return;
+
+  // Setting to zeros because snopt sparsity cannot change
+  if (!triplet_list_.empty())                                               // NOLINT
+    jac_block.setFromTriplets(triplet_list_.begin(), triplet_list_.end());  // NOLINT
+
+  // Calculate collisions
+  // const Eigen::VectorXd joint_vals0 = this->GetVariables()->GetComponent(position_vars_[0]->GetName())->GetValues();
+  // const Eigen::VectorXd joint_vals1 = this->GetVariables()->GetComponent(position_vars_[1]->GetName())->GetValues();
+  const Eigen::VectorXd joint_vals0 = position_vars_[0]->value();
+  const Eigen::VectorXd joint_vals1 = position_vars_[1]->value();
+
+  auto collision_data =
+      collision_evaluator_->CalcCollisionData(joint_vals0, joint_vals1, vars0_fixed_, vars1_fixed_, bounds_.size());
+
+  if (collision_data->gradient_results_sets.empty())
+    return;
+
+  /** @todo Should use triplet list and setFromTriplets */
+  const std::size_t cnt = std::min(collision_data->gradient_results_sets.size(), bounds_.size());
+  if (!vars0_fixed_)
+  {
+    for (std::size_t i = 0; i < cnt; ++i)
+    {
+      const trajopt_common::GradientResultsSet& r = collision_data->gradient_results_sets[i];
+      if (r.max_error[0].has_error[0] || r.max_error[1].has_error[0])
+      {
+        double max_error_with_buffer = r.getMaxErrorWithBufferT0();
+        if (!vars1_fixed_)
+          max_error_with_buffer = r.getMaxErrorWithBuffer();
+
+        Eigen::VectorXd grad_vec = getWeightedAvgGradientT0(r, max_error_with_buffer, position_vars_[0]->size());
+
+        // Collision is 1 x n_dof
+        for (int j = 0; j < n_dof_; j++)
+          jac_block.coeffRef(static_cast<int>(i), j) = -1.0 * grad_vec[j];
+      }
+    }
+  }
+
+  if (!vars1_fixed_)
+  {
+    for (std::size_t i = 0; i < cnt; ++i)
+    {
+      const trajopt_common::GradientResultsSet& r = collision_data->gradient_results_sets[i];
+      if (r.max_error[0].has_error[1] || r.max_error[1].has_error[1])
+      {
+        double max_error_with_buffer = r.getMaxErrorWithBufferT1();
+        if (!vars0_fixed_)
+          max_error_with_buffer = r.getMaxErrorWithBuffer();
+
+        Eigen::VectorXd grad_vec = getWeightedAvgGradientT1(r, max_error_with_buffer, position_vars_[1]->size());
+
+        // Collision is 1 x n_dof
+        for (int j = 0; j < n_dof_; j++)
+          jac_block.coeffRef(static_cast<int>(i), j) = -1.0 * grad_vec[j];
+      }
+    }
+  }
+}
+
+void ContinuousCollisionConstraint2::SetBounds(const std::vector<ifopt::Bounds>& bounds)
+{
+  assert(bounds.size() == 1);
+  bounds_ = bounds;
+}
+
+std::shared_ptr<ContinuousCollisionEvaluator> ContinuousCollisionConstraint2::GetCollisionEvaluator() const
 {
   return collision_evaluator_;
 }
