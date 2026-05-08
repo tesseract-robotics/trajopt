6d955d0d0b9b4c1bf1039479fbe034976d9c56dd
John Wason <wason@wasontech.com>
2026-05-08 08:45:07 -0400

    Update conda build (#561)

--- .github/workflows/conda/recipe/build.sh
+++ .github/workflows/conda/recipe/build.sh
@@ -1,11 +1,12 @@
 set -e
 
-ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-gcc $BUILD_PREFIX/bin/gcc
+# ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-gcc $BUILD_PREFIX/bin/gcc
 
 colcon build --merge-install --install-base="$PREFIX/opt/tesseract_robotics" \
-   --event-handlers console_cohesion+  \
-   --packages-ignore gtest osqp osqp_eigen tesseract_examples trajopt_ifopt trajopt_sqp \
-   --cmake-args -DCMAKE_BUILD_TYPE=Release \
+   --event-handlers console_direct+  \
+   --packages-ignore gtest osqp osqp_eigen tesseract_examples vhacd \
+   --cmake-args -GNinja \
+   -DCMAKE_BUILD_TYPE=Release \
    -DBUILD_SHARED_LIBS=ON \
    -DBUILD_IPOPT=OFF \
    -DBUILD_SNOPT=OFF \
@@ -13,18 +14,21 @@ colcon build --merge-install --install-base="$PREFIX/opt/tesseract_robotics" \
    -DTESSERACT_ENABLE_CLANG_TIDY=OFF \
    -DTESSERACT_ENABLE_CODE_COVERAGE=OFF \
    -DTESSERACT_ENABLE_EXAMPLES=OFF \
-   -DTESSERACT_BUILD_TRAJOPT_IFOPT=OFF \
+   -DTESSERACT_BUILD_TRAJOPT_IFOPT=ON \
    -DSETUPTOOLS_DEB_LAYOUT=OFF \
-   -DTESSERACT_ENABLE_TESTING=ON \
+   -DTESSERACT_ENABLE_TESTING=OFF \
    -DTRAJOPT_ENABLE_TESTING=ON \
    -DTRAJOPT_ENABLE_BENCHMARKING=ON \
-   -DTRAJOPT_ENABLE_RUN_BENCHMARKING=OFF
+   -DTRAJOPT_ENABLE_RUN_BENCHMARKING=OFF \
+   -DTESSERACT_WARNINGS_AS_ERRORS=OFF \
+   -DTRAJOPT_WARNINGS_AS_ERRORS=OFF \
+   -DCMAKE_VERBOSE_MAKEFILE=ON
 
 export TESSERACT_RESOURCE_PATH="$PREFIX/opt/tesseract_robotics/share"
 
 colcon test --event-handlers console_direct+  \
    --return-code-on-test-failure \
-   --packages-ignore gtest osqp osqp_eigen tesseract_examples trajopt_ifopt trajopt_sqp tesseract_common \
+   --packages-ignore gtest osqp osqp_eigen tesseract vhacd \
    --merge-install --install-base="$PREFIX/opt/tesseract_robotics" 
 
 
